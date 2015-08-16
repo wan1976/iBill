7d0640d3067a2a0ab57db8b8e861c3a7aa5fae94
wan1976 <tony@toeach.net>
2015-08-16 16:49:05 +0800

    我的账单： 1、增加下拉菜单功能；

--- app/src/main/java/net/toeach/ibill/ui/fragment/BillFragment.java
+++ app/src/main/java/net/toeach/ibill/ui/fragment/BillFragment.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.lidroid.xutils.ViewUtils;
+import com.shehabic.droppy.DroppyClickCallbackInterface;
+import com.shehabic.droppy.DroppyMenuPopup;
 
 import net.toeach.ibill.R;
 
@@ -18,10 +20,8 @@ public class BillFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.bill_fragment_layout, container, false);
         ViewUtils.inject(this, view);
-
         initView(view);
         init();
-
         return view;
     }
 
@@ -32,11 +32,26 @@ public class BillFragment extends BaseFragment {
     private void init() {
         hideBackButton();// 隐藏返回按钮
         setTitleValue(R.string.bill_title);// 设置标题名称
-        setFuncButton(R.drawable.button_add);// 设置功能按钮
+        setFuncButton(R.drawable.button_more);// 设置功能按钮
     }
 
     @Override
     public void doFunction() {
-
+        DroppyMenuPopup.Builder builder = new DroppyMenuPopup.Builder(getActivity(), mBtnFunc);
+        DroppyMenuPopup mMenu = builder.fromMenu(R.menu.bill_menu)
+                .triggerOnAnchorClick(false)
+                .setOnClick(new DroppyClickCallbackInterface() {
+                    @Override
+                    public void call(View v, int id) {
+                        switch (id) {
+                            case R.id.menu_1:// 月账单
+                                break;
+                            case R.id.menu_2:// 自定义账单
+                                break;
+                        }
+                    }
+                })
+                .build();
+        mMenu.show();
     }
 }
